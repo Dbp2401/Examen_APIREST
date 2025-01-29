4d130b06762f91a8f3b5579ce018ca75ed9af531
Dbp2401 <dbp24012003@gmail.com>
2025-01-29 18:34:08 +0100

    default structure

--- main.ts
+++ main.ts
@@ -1,20 +1,47 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { MongoClient, ObjectId } from "npm:mongodb";
 
-const handler = (req: Request): Response => {
-  const url = new URL(req.url);
 
-  if (url.pathname === "/") {
-    return new Response("Hello, Deno Deploy! 🚀");
+const MONGO_URL = Deno.env.get("MONGO_URL")
+
+if (!MONGO_URL) {
+  console.log(MONGO_URL)
+  throw new Error("La variable de entorno 'MONGO_URL' no está configurada");
+}
+
+const client = new MongoClient(MONGO_URL);
+await client.connect();
+console.log('Conectado a la base de datos');
+const db = await client.db('Nombre Base de Datos');
+
+//const xCollection = db.collection<>('nombreColeccion');kids
+
+const apiNinjaKey = Deno.env.get('apiNinjaKey')
+
+if (!apiNinjaKey) {
+  console.error('Api key inválida');
+}
+
+
+const handler = async(req : Request):Promise<Response> =>{
+  const method = req.method
+  const url = new URL(req.url)
+  const path = url.pathname
+
+  if(method === "GET") {
+    //TODO
+  }
+  else if (method  === "POST"){
+    //TODO
+  }
+  else if (method === "PUT"){
+    //TODO
+  }
+  else if (method === "DELETE"){
+    //TODO
   }
 
-  if (url.pathname === "/users") {
-    return new Response(JSON.stringify([{ id: 1, name: "Dani" }]), {
-      headers: { "Content-Type": "application/json" },
-    });
-  }
+  return new Response('Endpoint not found', { status: 404 });
+}
 
-  return new Response("Not Found", { status: 404 });
-};
 
-console.log("Server running on Deno Deploy...");
-serve(handler);
+Deno.serve({ port: 8080 }, handler);
